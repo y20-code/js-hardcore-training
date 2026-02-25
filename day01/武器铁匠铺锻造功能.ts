3b6d56c47c280f26f6069082f7df04d02c32d49b
y20-code <yls3235882107@gmail.com>
2026-02-25 19:28:59 +0800

    feat:实现武器铺锻造功能

---
+++ day01/武器铁匠铺锻造功能.ts
@@ -0,0 +1,27 @@
+const forgeSystem = {
+    smithName:"欧冶子",
+    shopLevel:1,
+    upgradeWeapon:function(atack:number,cost=50,...energy:number[]){
+
+        const totalEnergy = energy.reduce((sum,item) => sum + item,0)
+        const finalAttack =  atack + totalEnergy + (this.shopLevel * 10)
+        return  {
+            finalAttack,
+            cost
+        }
+    },
+    broadcastAd:function(){
+        setTimeout(() =>{
+            console.log(`我是${this.smithName},我的铁匠铺目前是${this.shopLevel}级,欢迎来打造神器`)
+        },2000)
+    }
+} 
+
+
+// 测试 1：玩家拿一把 100 攻击的剑，没传手续费，放了三块材料（10, 20, 30 能量）
+const weaponCard = forgeSystem.upgradeWeapon(100, undefined, 10, 20, 30);
+console.log("锻造结果：", weaponCard);
+
+// 测试 2：触发打广告
+forgeSystem.broadcastAd();
+// 预期：安静 2 秒后，突然打印：“我是 欧冶子，我的铁匠铺目前是 1 级，欢迎来打造神器！”
